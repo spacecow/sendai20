d04ccef1120df8fb711368bbccd1146f97e7d31b
Johan Sveholm <jsveholm@gmail.com>
2011-04-28 15:12:56 +0900

    updated javascript for updating with more than one opinion

--- app/controllers/operator_controller.rb
+++ app/controllers/operator_controller.rb
@@ -6,7 +6,7 @@ class OperatorController < ApplicationController
     
     @opinion = (current_user && current_user.opinion) || Opinion.new
     
-    @polled_opinions = Opinion.where("updated_at > ?", Time.at(params[:after].to_i)).order("updated_at asc")
+    @polled_opinions = Opinion.where("updated_at > ?", Time.at(params[:after].to_i)).order("updated_at desc")
     @polled_opinions = @polled_opinions.where("user_id <> ?",current_user.id) if current_user
   end
 end
